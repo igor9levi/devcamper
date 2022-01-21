1684e18a7dafa59e2d34b8b9a22700ff5a8ac20a
Igor Radovic <igor9levi@gmail.com>
2022-01-21 02:46:04 +0100

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Routes
 const bootcamps = require('./routes/bootcamps');
@@ -9,18 +10,29 @@ dotenv.config();
 
 const app = express();
 
-// Mount routes
-app.use('/api/v1/bootcamps', bootcamps);
+// Connect to database
+connectDB();
 
-// Middlewares
+// Middlewares - MUST BE ORDERED before routes !!!
+// Reads NODE_ENV from .env
 if (process.env.NODE_ENV === 'dev') {
   app.use(morgan('dev'));
 }
 
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(
     `Server running in ${process.env.NODE_ENV} mode, listening on port: ${PORT}`
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+});
